f28a967d4f8e7680abeb2459cb6d5efcbba4bda3
leemendoza <leemendoza@gmail.com>
2014-10-10 08:35:46 -0500

    updated script

--- run_analysis.R
+++ run_analysis.R
@@ -22,3 +22,23 @@
 ## Appropriately labels the data set with descriptive variable names.
 
 ## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+
+## get to the working directory
+setwd("c:\\github\\tidy_data")
+
+######################################################################
+### do this only once!! I've already done it, so don't do it again!!
+# if (!file.exists(".\\data")){
+#     dir.create(".\\data")
+# }
+# 
+### the source data set is located here. unzip into the .\data folder
+# download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = ".\\data\\dataset.zip", mode = "wb")
+## the file describing the data is found here
+# browseURL("http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones")
+
+## the reference for the data is
+## Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and 
+## Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones 
+## using a Multiclass Hardware-Friendly Support Vector Machine. International 
+## Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
